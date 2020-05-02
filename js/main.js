67c7c34c506e38e75422b890a06d29cad514f2ee
geard <53234590+geard-dev@users.noreply.github.com>
2020-05-02 06:25:56 -0400

    Update main.js
    Changed the default prewritten code

--- js/main.js
+++ js/main.js
@@ -14,12 +14,12 @@ function setupEditor()
   editor.getSession().setMode("ace/mode/html");
   editor.setValue(`<!DOCTYPE html>
 <html>
-<head>
-</head>
-
-<body>
-</body>
-
+  <head>
+    
+  </head>
+  <body>
+    
+  </body>
 </html>`,1); //1 = moves cursor to end
 
   editor.getSession().on('change', function() {
